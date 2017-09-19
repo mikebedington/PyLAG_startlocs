2f4aa649f65383ba3866a8a05f829e6ff3d8299d
Mike Bedington <mbe@pml.ac.uk>
2017-09-19 15:30:04 +0100

    Get parallel pickin workin

--- example_run.py
+++ example_run.py
@@ -4,12 +4,8 @@ from shapely.geometry import Polygon, Point
 
 import pylag_startlocs as ps
 
-fvcom_grid_file = '/data/euryale4/backup/mbe/Code/fvcom-projects/locate/run/input/tamar_v2_grd.dat'
-triangle, nodes, X, Y, Z = pf.grid_tools.read_fvcom_mesh(fvcom_grid_file)
-poly_list = pf.grid_tools.get_boundary_polygons(triangle)
-poly_xy = np.asarray([X[poly_list[0]], Y[poly_list[0]]]).T
 
-tamar_subset = np.asarray([[420437.66909469309, 5578342.1487603299],
+example_poly_points = np.asarray([[420437.66909469309, 5578342.1487603299],
 						[422051.53485952143, 5578877.6859504124],
 						[423096.57908428728, 5581793.3884297507],
 						[423123.03590010415, 5584917.3553718999],
@@ -22,11 +18,44 @@ tamar_subset = np.asarray([[420437.66909469309, 5578342.1487603299],
 						[415662.21383975033, 5578074.3801652882],
 						[416746.94328824151, 5578312.3966942141]])
 
+# First instantiate a grid_area object of the polygon we're interestd in
+# We can pass it either a list/array of points or a shapely Polygon object
+tamar_subset_area = ps.grid_area(example_poly_points)
+
+# Now create a start_locs_set instance with this grid area
+test_start_locs = ps.start_locs_set(tamar_subset_area)
+
+# And make some particle locatations
+print('Serial example')
+test_start_locs.get_n_particles(10000) 
+test_start_locs.write_particle_file('test_out.dat')
+print('Parallel example')
+test_start_locs.get_n_particles(100000,serial=False,poolsize=8) 
+test_start_locs.write_particle_file('test_parallel_out.dat')
+
+
+
+"""
+Code for getting the tamar estuary outline which I haven't parked elsewhere yet...
+
+fvcom_grid_file = '/data/euryale4/backup/mbe/Code/fvcom-projects/locate/run/input/tamar_v2_grd.dat'
+triangle, nodes, X, Y, Z = pf.grid_tools.read_fvcom_mesh(fvcom_grid_file)
+poly_list = pf.grid_tools.get_boundary_polygons(triangle)
+poly_xy = np.asarray([X[poly_list[0]], Y[poly_list[0]]]).T
+
+tamar_subset = np.asarray([[420437.66909469309, 5578342.1487603299],
+                        [422051.53485952143, 5578877.6859504124],
+                        [423096.57908428728, 5581793.3884297507],
+                        [423123.03590010415, 5584917.3553718999],
+                        [418056.55567117594, 5598395.0413223132],
+                        [411574.63579604583, 5599049.5867768582],
+                        [407698.71227887622, 5591462.809917354],
+                        [407196.03277835593, 5582507.4380165283],
+                        [408796.67013527581, 5580038.0165289249],
+                        [414431.97190426645, 5577211.5702479333],
+                        [415662.21383975033, 5578074.3801652882],
+                        [416746.94328824151, 5578312.3966942141]])
+
 rough_subset_area = ps.grid_area(tamar_subset)
 is_in = rough_subset_area.points_in_area(poly_xy)
-
-tamar_subset_area = ps.grid_area(poly_xy[is_in,:])
-
-test_start_locs = ps.start_locs_set(tamar_subset_area)
-test_start_locs.get_n_particles(10000)
-test_start_locs.write_particle_file('test_out.dat')
+"""
--- pylag_startlocs/utils.py
+++ pylag_startlocs/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as r
+import multiprocessing as mp
 from shapely.geometry import Polygon, Point
 
 import PyFVCOM as pf
@@ -7,27 +8,45 @@ import PyFVCOM as pf
 # TODO Parrerererelalise
 
 MAX_PARTS_AT_ONCE = 2000
+PARTS_PER_PLL_TASK = 4000
 
 class start_locs_set():
 	def __init__(self, choose_area):
 		self.choose_area = choose_area
 		self.bounding_box = choose_area.area_bound_box
 
-	def get_n_particles(self, n):
-		parts_per_it = min(MAX_PARTS_AT_ONCE, n)
+	def get_n_particles(self, n, serial=True, poolsize=4):
+		if serial:
+			parts_per_it = min(MAX_PARTS_AT_ONCE, n)
+		else:
+			parts_per_it = list(np.asarray(np.ones(poolsize)*np.floor(min(PARTS_PER_PLL_TASK,n)/poolsize), dtype=int))
 
 		chosen_parts = np.asarray([[],[]]).T
+		if not serial:
+			pool = mp.Pool(poolsize)
 	
 		while len(chosen_parts) < n:
+			if serial:
+				chosen_parts = np.append(chosen_parts, self._try_n_parts(parts_per_it), axis=0)
+			else:
+				# Launch the parallel plotting and then close the pool ready for the
+				# next variable.
+				chosen_parts_raw = pool.map(self._try_n_parts, parts_per_it)
+				chosen_parts_raw = np.asarray([item for sublist in chosen_parts_raw for item in sublist])
+				chosen_parts = np.append(chosen_parts, chosen_parts_raw, axis=0)
 			print("%d of %d particles chosen" % (len(chosen_parts), n))
-			x_parts = [r.uniform(self.bounding_box[0,0], self.bounding_box[0,1]) for x in range(0, parts_per_it)]
-			y_parts = [r.uniform(self.bounding_box[1,0], self.bounding_box[1,1]) for x in range(0, parts_per_it)]
-			parts_xy = np.asarray([x_parts, y_parts]).T
-			parts_in = self.choose_area.points_in_area(parts_xy)
-			chosen_parts = np.append(chosen_parts, parts_xy[parts_in], axis=0)
-		
-		self.chosen_parts = chosen_parts[0:n]
+	
+		if not serial:
+			pool.close()
+		self.chosen_parts = chosen_parts
 		return self.chosen_parts
+
+	def _try_n_parts(self, n):
+		x_parts = [r.uniform(self.bounding_box[0,0], self.bounding_box[0,1]) for x in range(0, n)]
+		y_parts = [r.uniform(self.bounding_box[1,0], self.bounding_box[1,1]) for x in range(0, n)]
+		parts_xy = np.asarray([x_parts, y_parts]).T
+		parts_in = self.choose_area.points_in_area(parts_xy)
+		return parts_xy[parts_in,:]
 
 	def write_particle_file(self, filename, depth=0):			
 		with open(filename, 'w') as out_file:
@@ -39,7 +58,10 @@ class start_locs_set():
 
 class grid_area():
 	def __init__(self, outline_points):
-		self.area_poly = Polygon(tuple(map(tuple, outline_points)))
+		if type(outline_points) == Polygon:
+			self.area_poly = outline_points
+		else:
+			self.area_poly = Polygon(tuple(map(tuple, outline_points)))
 		pb_temp = self.area_poly.bounds
 		self.area_bound_box = np.asarray([[pb_temp[0], pb_temp[2]],[pb_temp[1], pb_temp[3]]])
 
